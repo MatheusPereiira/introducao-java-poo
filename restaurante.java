ab10a993162959248597599209249b92d0254e7c
Matheus Pereira <tuliocorreamatheus@gmail.com>
2026-03-12 17:46:12 -0300

    feat: adiciona exercício 01 - cálculo do valor da refeição por quilo

---
+++ restaurante.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class restaurante {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    double kg, pesoPrato, valorAPagar;
+    kg = 32.50;
+
+    System.out.print("O peso do prato em quilos é de: ");
+    pesoPrato = sc.nextDouble();
+
+    valorAPagar = (kg * pesoPrato);
+    System.out.println("O valor a pagar é de: R$ " + valorAPagar);
+
+    sc.close();
+
+  }
+}
